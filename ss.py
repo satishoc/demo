621c5978e7c7806cc030095d1689daa2382722b0
satish kumar <42333978+satishoc@users.noreply.github.com>
2019-12-10 16:04:51 +0530

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,4 +1,4 @@
 from dkube.sdk.dkube import *
 
-env = Environment(scheme='https', host='192.168.200.23', user='ocdkube', token=sys.argv[1], port=32222)
+env = Environment(scheme='https', host='18.236.126.102', user='ocdkube', token=sys.argv[1], port=32222)
 launch_training_job("test", autogenerate=True, environ=env.external, workspace='mn-ws', script='python model.py',datasets=['mn-data'])
